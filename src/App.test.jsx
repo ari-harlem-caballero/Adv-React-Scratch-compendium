e2115a67d8d492eeddc8cda6e3fc31efa8fc7933
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-26 17:29:49 -0700

    wrote App Test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,1 +1,17 @@
 // check list displays, loading
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('should render out a list of Ghibli films', async () => {
+    render(<App />)
+
+    // loading message
+    screen.getByText(/loading/i);
+
+    // load 10 films per page
+    const film = await screen.findByAltText('film');
+
+    expect(film).toBeInTheDocument();
+  })
+})
