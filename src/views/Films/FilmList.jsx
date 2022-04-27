55442ad14883388c9ece57694d0f5631ca81b366
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-04-26 17:44:59 -0700

    set state

--- src/views/Films/FilmList.jsx
+++ src/views/Films/FilmList.jsx
@@ -1,5 +1,14 @@
 // useEffect/state
-// allFilms, loading, search, results(filtered)
-// event: search .filter
-// useEffect: fetch api, setData, map (id, img, title)
-// return html, search bar, mapped list
+import { useEffect, useState } from "react";
+
+export default function FilmList() {
+  // allFilms, loading, search, results(filtered)
+  const [films, setFilms] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  // event: search .filter
+  // useEffect: fetch api, setData, map (id, img, title)
+  // return html, search bar, mapped list
+
+}
